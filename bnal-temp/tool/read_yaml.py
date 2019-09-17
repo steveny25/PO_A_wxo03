59a973387a32dbf80378a41bfa634bb0867d82f7
steveny25 <steveny25@hotmail.com>
2019-09-17 19:51:16 +0800

    add bnal-temp

---
+++ bnal-temp/tool/read_yaml.py
@@ -0,0 +1,14 @@
+import yaml
+
+
+def read_yaml(filename):
+    with open("./data/"+filename,'r',encoding="utf-8")as f:
+        return yaml.load(f)
+
+if __name__ == '__main__':
+    print(read_yaml("login.yaml"))
+    print("--" * 50)
+    arrs = []
+    for data in read_yaml("login.yaml").values():
+        arrs.append(tuple(data.values()))
+    print(arrs)
